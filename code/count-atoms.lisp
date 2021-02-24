1ba1ff5e5744074b5ab0713b2fa669ff1e32e3cc
adricode <acpsvzla@outlook.com>
2021-02-24 13:15:46 +0000

    Add comments

--- code/count-atoms.lisp
+++ code/count-atoms.lisp
@@ -1,7 +1,9 @@
 ;;; Function that counts the number of atoms in an expression.
 
-(defun eval. (var) 
+(defun eval. (var)
+    "Assign 1 if it is an atom, 0 otherwise" 
     (if (atom var) (digit-char-p #\1) (digit-char-p #\0)))
 
-(defun count-atoms (var) 
+(defun count-atoms (var)  
+    "Evaluate each item and sum all results"
     (apply #'+ (mapcar #'eval. var)))
